721c7a2c8c7dabe6ffe9d727e9388639b87b354b
MoeBrowne <oliver.browne@uplyme.com>
2015-10-16 22:55:55 +0100

    Borrowed some code to init a canvas from my hex-grid project

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 
 var hexplosion = {
 
+	context: null,
+	canvas: null,
+
+	// Set everything up
+	init: function(canvasObject) {
+
+		// Add the canvas object to the hexGrid object
+		this.canvas = canvasObject;
+
+		// Get a drawing context
+		this.context = this.canvas.getContext('2d');
+	}
+
 };
