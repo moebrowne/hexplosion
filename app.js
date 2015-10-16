a0641b11695f45f59ada9313dd2115b809f755f6
MoeBrowne <oliver.browne@uplyme.com>
2015-10-16 23:22:02 +0100

    Added a method to draw a hexagon to the canvas

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ var hexplosion = {
 
 		// Get a drawing context
 		this.context = this.canvas.getContext('2d');
-	}
+	},
+
+	/**
+	 * Draw a hexagon onto the canvas
+	 *
+	 * @param ctx
+	 * @param x
+	 * @param y
+	 * @param radius
+	 */
+	drawHexagon: function (ctx, x, y, radius) {
+		var a = (Math.PI * 2)/6;
+		this.context.beginPath();
+		this.context.translate(x,y);
+		this.context.moveTo(radius,0);
+		for (var i = 1; i < 6; i++) {
+			this.context.lineTo(radius*Math.cos(a*i),radius*Math.sin(a*i));
+		}
+		this.context.closePath();
+		this.context.stroke();
+	},
 
 };
